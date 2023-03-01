0aac6ea138817afe6fbcb852cb591764ff8edb3c
ge-arty <115204381+ge-arty@users.noreply.github.com>
2023-03-01 23:21:08 +0400

    added-showRecipe-func

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -568,6 +568,29 @@ const timeout = function(s) {
 // https://forkify-api.herokuapp.com/v2
 ///////////////////////////////////////
 console.log("test");
+const showRecipe = async function() {
+    try {
+        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`);
+        const data = await res.json();
+        console.log(res, data);
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        let { recipe  } = data.data;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        };
+        console.log(recipe);
+    } catch (err) {
+        console.log(err);
+    }
+};
+showRecipe();
 
 },{}]},["d8XZh","aenu9"], "aenu9", "parcelRequire3a11")
 
