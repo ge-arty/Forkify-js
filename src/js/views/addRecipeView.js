559e808ed0473c374c7884f54166b964261efb97
ge-arty <george.arutinyan@gmail.com>
2023-03-05 20:29:53 +0400

    finish

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -1,29 +1,36 @@
-import View from './View';
-import icons from '../../img/icons.svg';
-class addRecipeView extends View {
+import View from './View.js';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
+class AddRecipeView extends View {
   _parentElement = document.querySelector('.upload');
+  _message = 'Recipe was successfully uploaded :)';
+
   _window = document.querySelector('.add-recipe-window');
   _overlay = document.querySelector('.overlay');
   _btnOpen = document.querySelector('.nav__btn--add-recipe');
   _btnClose = document.querySelector('.btn--close-modal');
+
   constructor() {
     super();
     this._addHandlerShowWindow();
     this._addHandlerHideWindow();
   }
+
   toggleWindow() {
-    this._window.classList.toggle('hidden');
     this._overlay.classList.toggle('hidden');
+    this._window.classList.toggle('hidden');
   }
 
   _addHandlerShowWindow() {
     this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
   }
+
   _addHandlerHideWindow() {
     this._btnClose.addEventListener('click', this.toggleWindow.bind(this));
     this._overlay.addEventListener('click', this.toggleWindow.bind(this));
   }
-  _addHandlerUpload(handler) {
+
+  addHandlerUpload(handler) {
     this._parentElement.addEventListener('submit', function (e) {
       e.preventDefault();
       const dataArr = [...new FormData(this)];
@@ -35,4 +42,4 @@ class addRecipeView extends View {
   _generateMarkup() {}
 }
 
-export default new addRecipeView();
+export default new AddRecipeView();
